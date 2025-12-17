21d7acad36c4b09564c265927901937d75fdbeac
Steven V <steven.verard@gmail.com>
2025-12-17 10:11:39 +0100

    fin des exo34

--- AlgoConditions/Partie3/Exo33RechercherNbDsTab.java
+++ AlgoConditions/Partie3/Exo33RechercherNbDsTab.java
@@ -1,7 +1,29 @@
 package AlgoConditions.Partie3;
 
+import java.util.Scanner;
+
 public class Exo33RechercherNbDsTab {
     public static void main(String[] args) {
-        
+        int n=0;
+        boolean b = false;
+        int [] nb = {1,9,17,25,33,41,49,57,65,73,81,89,97,105,113,121,129,137,145,153,161,169,177,185,193,201,209,217,225,233,241,249};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Saisir nb");
+        n = sc.nextInt();
+
+        for(int i =0; i<nb.length;i++  ){
+            
+            if (nb [i] == n){
+                System.out.println("l'emplacement du nb est dans la case : "+i);
+                b=true;
+                break;
+            }
+        }
+    
+        if(!b){
+            System.out.println("404 Not found ");
+        }
+
     }
 }
---
+++ AlgoConditions/Partie3/Exo34lettredansunechaine.java
@@ -0,0 +1,32 @@
+package AlgoConditions.Partie3;
+
+import java.util.Scanner;
+
+public class Exo34lettredansunechaine {
+    public static void main(String[] args) {
+
+
+        String p="je suis un licorne qui fais de la programmation";
+        char l;
+        Scanner sc = new Scanner(System.in);
+        int cpt = 0;
+
+        System.out.println("Saisir une lettre");
+        l = sc.next().charAt(0);
+
+        for(int i=0; i<p.length();i++) {
+            
+            if(p.charAt(i) == l){
+                cpt++;  
+            }
+             
+        }
+        if(cpt!=0){
+            System.out.println("vous avez trouvé la lettre : "+cpt);
+        }else{
+            System.out.println("il n'y a pas cette lettre ds la phrase");          
+        }
+        
+
+    }
+}
