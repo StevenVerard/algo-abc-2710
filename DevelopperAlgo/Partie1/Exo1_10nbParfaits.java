5b0be5e9f222dc1d770a9746e9e2765f6da60dbf
Steven V <steven.verard@gmail.com>
2025-12-22 16:39:29 +0100

    10parfait

--- DevelopperAlgo/Partie1/Exo1_10nbParfaits.java
+++ DevelopperAlgo/Partie1/Exo1_10nbParfaits.java
@@ -2,20 +2,18 @@ package DevelopperAlgo.Partie1;
 import java.util.Scanner;
 public class Exo1_10nbParfaits {
     public static void main(String[] args) {
-        int i=2;
-        int nb, test;
+        int i=0;
+        int nb, test, cpt=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("nb : ");
         nb = sc.nextInt();
-
-        while(i<=nb/2){
-            test=nb%i;
-            if(test==0){
-                System.out.println("Les diviseurs sont : "+i);
-                
-            }
+        while(i<nb){
+            
+            System.out.println("Affichage des "+nb+" premiers nombrs parfaits");
+            System.out.println("est  : "+i);  
             i++;
-        } 
+            
+        }
         sc.close();
 
 
