94865423734d434006d400e8b86c08d29a6feb73
Steven V <steven.verard@gmail.com>
2026-02-09 16:58:08 +0100

    tp-fraction

---
+++ TP-Fraction/src/AppFracion.java
@@ -0,0 +1,7 @@
+public class AppFracion {
+    public static void main(String[] args) throws Exception {
+    Fraction f1 = new Fraction ( 12, 7); // dans ce cas le numérateur vaudra 12 et le dénominateur 7
+    Fraction f2 = new Fraction(); // fraction nulle de dénominateur 1
+    Fraction f3 = new Fraction ( 9 ); // fraction dont le numérateur vaut 9 et le dénominateur 1
+    }
+}
---
+++ TP-Fraction/src/Fraction.java
@@ -0,0 +1,12 @@
+public class Fraction {
+    
+    public double numerateur;
+    public double denominateur;
+    public Fraction (){
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    
+
+}
